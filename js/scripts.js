15a27a10cfd48f119bdc995b455cf48863a8a229
Manar Kurmanov <kurmanov.manar@gmail.com>
2016-08-26 03:38:05 +0600

    scrubs and friends added

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+function himym() {
+    var season = getRandomInt(1, 10);
+    var episode = function() {
+        if (season == 3 ||  season == 6) {
+            return getRandomInt(1, 25)
+        } else if(season == 10){
+            return getRandomInt(1, 18)
+        } else {
+            return getRandomInt(1, 24)
+        }
+    }
+    document.getElementById('result').innerHTML = "Season: " + season + "<br> Episode: " + episode()
+}
+function friends() {
+    var season = getRandomInt(1, 9);
+    var episode = function() {
+        if (season == 3) {
+            return getRandomInt(1, 20)
+        } else {
+            return getRandomInt(1, 24)
+        }
+    }
+    document.getElementById('result').innerHTML = "Season: " + season + "<br> Episode: " + episode()
+}
+
+function scrubs() {
+    var season = getRandomInt(1, 9);
+    var episode = function() {
+        switch (season) {
+            case 1:
+                return getRandomInt(1, 24)
+                break;            
+            case 2:
+                return getRandomInt(1, 22)
+                break;         
+            case 3:
+                return getRandomInt(1, 22)
+                break;     
+            case 4:
+                return getRandomInt(1, 25)
+                break;
+            case 5:
+                return getRandomInt(1, 24)
+                break;
+            case 6:
+                return getRandomInt(1, 22)
+                break;
+            case 7:
+                return getRandomInt(1, 11)
+                break;
+            case 8:
+                return getRandomInt(1, 19)
+                break;
+            case 9:
+                return getRandomInt(1, 13)
+                break;
+        }
+
+    }
+    document.getElementById('result').innerHTML = "Season: " + season + "<br> Episode: " + episode();
+    if(season == 9){
+        document.getElementById('result').innerHTML += "<br><span class='label label-danger'>Season 9 is not recommended</span>";
+    }
+}
+
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max + 1 - min)) + min;
+}
